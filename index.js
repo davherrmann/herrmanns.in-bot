84d020807598ea856ba0442a2354d7ba35198486
David Herrmann <davherrmann@googlemail.com>
2017-02-01 14:45:12 +0100

    fix bot builder usage, create update script

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const botBuilder = require('claudia-bot-builder')
 
-module.exports = (event, context) => {
-  console.log(JSON.stringify(event, null, 2))
-  console.log(JSON.stringify(context, null, 2))
+module.exports = botBuilder((request, originalRequest) => {
+  console.log(JSON.stringify(request, null, 2))
+  console.log(JSON.stringify(originalRequest, null, 2))
 
-  return botBuilder((request, originalRequest) => {
-    console.log(JSON.stringify(request, null, 2))
-    console.log(JSON.stringify(originalRequest, null, 2))
-
-    return `Hello from space explorer bot! Your request was: ${request.text}`
-  }, { platforms: ['telegram'] })(event, context)
-}
+  return `Hello from space explorer bot! Your request was: ${request.text}`
+}, { platforms: ['telegram'] })
